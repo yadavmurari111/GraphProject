fff068af39f065437f3071fd484f447ea61e9198
Murari Yadav <murari.yadav@thingmzing.com>
2023-08-02 02:43:54 +0530

    assessment done

--- App.tsx
+++ App.tsx
@@ -1,117 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {BarChart} from 'react-native-chart-kit';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View,} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const data = {
+  labels: ["1st", "2nd", "3rd"],
+  datasets: [{data: [25,30,45]}]
+};
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [chartData, setChartData] = useState(data);
+  const [marks, setMarks] = useState('');
+  const [termName, setTermName] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const handleAddItemUpdate =()=>{
+    setChartData({
+      labels: [...chartData.labels,termName],
+      datasets: [{data: [...chartData.datasets[0].data,Number(marks)]}]
+    })
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.sectionContainer}>
+      <View>
+        <BarChart
+          data={chartData}
+          width={350}
+          height={350}
+          segments={5}
+          fromZero={true}
+          yAxisLabel={''}
+          yAxisSuffix={''}
+          flatColor={true}
+          xLabelsOffset={5}
+          yLabelsOffset={8}
+          withInnerLines={true}
+          style={{borderRadius:16,margin:16}}
+          showValuesOnTopOfBars={true}
+          chartConfig={{
+            backgroundColor: "#830080",
+            backgroundGradientFrom: "#ffffff",
+            backgroundGradientTo: "#ffffff",
+            decimalPlaces: 0,
+            barRadius:6,
+            propsForBackgroundLines:{ },
+            linejoinType:'round',
+            color: () => `#FF1493`, // Change this to the desired color FF1493
+            labelColor: () => `black`,
+          }}
+        />
+      </View>
+
+      <View style={styles.btnContainer}>
+        <TextInput
+            keyboardType={'number-pad'}
+            onChangeText={setMarks}
+            placeholder={'Enter Marks'}
+            style={styles.inputField}
+        />
+        <TextInput
+            onChangeText={setTermName}
+            placeholder={'Enter Name'}
+            style={styles.inputField}/>
+        <TouchableOpacity
+            onPress={handleAddItemUpdate}
+            style={styles.addItem}>
+          <Text>Add item</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    flex: 1,
+    paddingHorizontal:18,
+    alignItems: 'center',
+    backgroundColor:'#89CFF0'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputField: {marginTop:12,margin:8,padding:5,borderWidth:.5,borderRadius:12,backgroundColor:'white'},
+  btnContainer: {flexDirection:'row',justifyContent:'space-evenly'},
+  addItem: {
+    marginTop:12,
+    flex: 1,
+    margin:8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'yellow',
+    borderRadius:12
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  gridContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  highlight: {
-    fontWeight: '700',
+  verticalGridLine: {
+    width: 1,
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+  },
+  horizontalGridLine: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
   },
 });
 
